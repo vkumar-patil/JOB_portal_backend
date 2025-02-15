74f9603c4de532779ebf3931e10b91a0cc25dbfc
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2025-02-15 23:40:31 +0530

    update

--- Controllers/usercontroler.js
+++ Controllers/usercontroler.js
@@ -28,7 +28,7 @@ exports.register = async (req, res) => {
 };
 
 exports.login = async (req, res) => {
-  const { email, password, Employer } = req.body;
+  const { email, password } = req.body;
 
   try {
     const user = await User.findOne({ email });
